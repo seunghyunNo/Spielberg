2194a7eb8d73d1fc755934d3d0005918a8edc485
june276 <jungjune276@gmail.com>
2023-08-18 21:33:06 +0900

    first

---
+++ src/test/java/com/project/MovieMania/MovieManiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MovieMania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
