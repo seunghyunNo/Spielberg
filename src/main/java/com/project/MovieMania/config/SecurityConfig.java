611b23e0f47431dd6ce017a16113c855c9571419
seunghyunNo <qpqpqp9508@gmail.com>
2023-08-25 21:05:14 +0900

    08/25 encoder 수정필요

--- src/main/java/com/project/MovieMania/config/SecurityConfig.java
+++ src/main/java/com/project/MovieMania/config/SecurityConfig.java
@@ -1,25 +1,30 @@
 package com.project.MovieMania.config;
 
+import com.project.MovieMania.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
+
+    private UserService userService;
+
 
     @Autowired
     private PrincipalDetailsService principalDetailsService;
 
-//    @Bean
-//    public PasswordEncoder encoder(){
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     //
     @Bean
