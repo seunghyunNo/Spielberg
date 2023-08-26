ae8fe312f919dd5646a8ce58609a46f6fd42862b
hyunsoo922 <hskang9200@naver.com>
2023-08-26 20:32:30 +0900

    Ticketing 인원수 == 좌석수 제한

--- src/main/resources/static/js/ticket/ticketing.js
+++ src/main/resources/static/js/ticket/ticketing.js
@@ -1,28 +1,26 @@
 $(function () {
     const showInfoId = $("#showInfoId").val();
     const userId = $("#userId").val();
-    var adultNum;
-    var studentNum;
+    var adultNum = 0;
+    var studentNum = 0;
+    var people = 0;
     $("#adult").change(function () {
         adultNum = parseInt($(this).val());
-        console.log(adultNum);
+        people += adultNum;
+        console.log(adultNum+"명");
     });
 
     $("#student").change(function () {
         studentNum = parseInt($(this).val());
-        console.log(studentNum);
+        people += studentNum;
+        console.log(studentNum+"명");
     });
 
     var cnt = 0;
-    var people = adultNum + studentNum;
-    alert(showInfoId);
+//    alert(showInfoId);
     // 처음 예약 좌석 / 미예약 좌석 확인
 
     // 예약된 좌석이면 버튼 클릭시 경고출력
-
-    // 가격 책정
-
-    // 남은것 인원수 == 자리수 확인   가격 측정 price처리 
 
     $("[data-column-row]").click(function () {
         let row = $(this).siblings("span").attr("data-seat-row");
@@ -49,10 +47,24 @@ $(function () {
                     crnObj.removeClass("seat");
                     crnObj.addClass("selectSeat");
                     cnt++;
+                    console.log("카운트"+cnt);
                     saveSeat(column, row, showInfoId);
                 }
             },
         });
+
+    });
+    $("#purchaseBtn").click(function(){
+        console.log("카운트"+cnt);
+        console.log("인원수"+people);
+        if(cnt == people)
+        {
+            $("[name='ticketingFrm']").submit();
+        }
+        else
+        {
+            alert("선택하신 인원수 만큼 좌석을 선택하십시오.");
+        }
 
     });
 
