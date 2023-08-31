d56cbb227542456177ada0038652c3c27402866d
hyunsoo922 <hskang9200@naver.com>
2023-08-31 22:14:29 +0900

    영화관별 상영 날짜 추가

--- src/main/resources/static/js/ticket/theater.js
+++ src/main/resources/static/js/ticket/theater.js
@@ -1,6 +1,40 @@
 $(function(){
+    let movieId = $("#movieId").val();
+    alert(typeof(movieId));
 	$("#cinemaName").change(function(){
+        let name = $(this).val();
+        let dateS = $("#date");
+        dateS.empty();
+        dateS.append(`<option selected>날짜 선택</option>`);
+        $.ajax({
+            url: "/ticketSelect/selectDate",
+            type: "POST",
+            cache: false,
+            data: {
+                "cinemaName": name,
+                "movieId": movieId,
+            },
+            success:function(data)
+            {
+                alert(data.length);
+                console.log(data);
+                for(var i = 0; i < data.length; i++)
+                {
+                    let date = data[i].trim();
+                    console.log(i);
+                    dateS.append(`<option value="${date}">${date}<option>`);
+                }
 
+                for(var x = 0; x < data.length; x++)
+                {
+                    let index = 2 * (x+1);
+                    $(`select[name=date] option:eq(${index})`).remove();
+                }
+                let lastIndex =$("select[name=date] option").length-1;
+                 $(`select[name=date] option:eq(${lastIndex})`).remove();
+                console.log(dateS.val());
+            },
+        });
 	});
 
 
